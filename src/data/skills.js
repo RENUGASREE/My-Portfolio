c0d92b479687242c2e5e08522f8505eadc28cc78
RENUGASREE <renugasree28@gmail.com>
2025-08-11 23:00:21 +0530

    Delete src directory

--- src/data/skills.js
+++
@@ -1,8 +0,0 @@
-const skills = {
-  "AI & Data Science": ["AI Models", "Data Analysis", "Machine Learning", "Deep Learning", "NLP", "Computer Vision", "Prompt Engineering"],
-  "Web Development": ["HTML", "CSS", "JavaScript", "ReactJS"],
-  "Tools": ["Git & GitHub", "VS Code"],
-  "Programming Languages": ["Python", "Java"],
-};
-
-export default skills;
